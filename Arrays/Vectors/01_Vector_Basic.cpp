27ba8310b57e4d1aadead427b26c8cb4ef176c64
Gopi Ram Sen <99002843+Gopisen777@users.noreply.github.com>
2024-01-31 16:17:56 +0530

    Add files via upload

---
+++ Arrays/Vectors/01_Vector_Basic.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+vector<int> v;
+
+v.push_back(2);
+v.push_back(21);
+v.push_back(21);
+v.push_back(21);
+v.push_back(21);
+v.push_back(21);
+v.push_back(21);
+v.push_back(21);
+v.push_back(21);
+// cout << v.capacity() << endl;
+
+cout << v.capacity() << endl;
+return 0;
+}
